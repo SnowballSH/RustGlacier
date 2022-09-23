592446af4a40182e66452dc1a19ad12264297a88
SnowballSH <derek070211@gmail.com>
2022-09-22 21:30:02 -0500

    push alloc to set

--- src/vm/memory.rs
+++ src/vm/memory.rs
@@ -1,15 +1,20 @@
 use std::alloc::{alloc, Layout};
-
+use std::collections::hash_set::HashSet;
+use std::sync::Mutex;
 use lazy_static::lazy_static;
 
 use crate::vm::value::Value;
 
+static LAYOUT: Layout = Layout::new::<Value>();
+
 lazy_static! {
-    static ref LAYOUT: Layout = Layout::new::<Value>();
+    pub static ref ALL_ALLOCATIONS: Mutex<HashSet<usize>> = Mutex::new(HashSet::new());
 }
 
 pub fn alloc_value_ptr() -> *mut Value {
-    unsafe { alloc(*LAYOUT) as *mut Value }
+    let ptr = unsafe { alloc(LAYOUT) as *mut Value };
+    ALL_ALLOCATIONS.lock().unwrap().insert(ptr as usize);
+    ptr
 }
 
 pub fn alloc_new_value(val: Value) -> *mut Value {
