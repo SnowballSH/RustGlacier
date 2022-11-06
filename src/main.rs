26015628b4fac7d3db9dac1a9081ff5e52ed2ba7
SnowballSH <derek070211@gmail.com>
2022-11-06 11:16:47 -0600

    remove debug info

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn main() {
 
                 vm.optimize();
 
-                println!("{}", vm.disassemble());
+                // println!("{}", vm.disassemble());
 
                 vm.execute();
                 if let Some(e) = &vm.error {
